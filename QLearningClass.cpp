b6526d9affb54160ec209922cb9433e9135e2b18
neoindian <neoindian@tamu.edu>
2015-03-02 21:32:50 -0600

    First commit.

---
+++ QLearningClass.cpp
@@ -0,0 +1,37 @@
+/*
+* Author: Nishant Pattanaik
+*
+* File  :QLearningClass.cpp
+*
+* Date C: March 2 , 2015
+*
+*
+* Description: This file contains the class declarations for
+*              holding the Q Learning parameters. 
+*
+*/
+
+
+#ifndef __QLEARNING_CLASS__
+#define __QLEARNING_CLASS__
+
+#include <iostream>
+#include <strings>
+#include <vector>
+
+using namespace std;
+
+class QLearningClass
+{
+  private:
+    std::vector<int> values_;
+    std::vector<string> nextState_;
+    std::vector<string> direction_;
+
+  public:
+    
+
+}
+
+
+#endif 
